9448aee1c13859102235c993412b7d1782de95bf
rh-pr <ruslana.hural.j@gmail.com>
2023-11-26 00:51:58 +0200

    conected addLaunch request with frontEnd

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -15,20 +15,19 @@ function httpAddNewLaunch (req, res) {
     !launch.mission ||
     !launch.rocket ||
     !launch.launchDate ||
-    !launch.destination
+    !launch.target
   ) {
     return res.status (400).json ({
       error: 'Missing required launch property',
     });
   }
 
+  launch.launchDate = new Date (launch.launchDate);
   if (isNaN (launch.launchDate)) {
     return res.status (400).json ({
       error: 'Invalid launch date',
     });
   }
-
-  launch.launchDate = new Date (launch.launchDate);
   addNewLaunches (launch);
   return res.status (200).json (launch);
 }
