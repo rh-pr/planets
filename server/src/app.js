c2e8cc4f5558c1f628bf3e3985e9c8055961d7e4
rh-pr <ruslana.hural.j@gmail.com>
2023-11-25 09:36:05 +0200

    created router for launches request

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,10 @@
 const express = require ('express');
 const cors = require ('cors');
 const path = require ('path');
+const morgan = require ('morgan');
 
 const planetsRouter = require ('./routes/planets/planets.router');
+const launchesRouter = require ('./routes/launches/launches.router');
 
 const app = express ();
 
@@ -11,11 +13,14 @@ app.use (
     origin: 'http://localhost:3000',
   })
 );
+app.use (morgan ('combined'));
+
 //parse any incomming json
 app.use (express.json ());
 app.use (express.static (path.join (__dirname, '..', 'public')));
 
 app.use (planetsRouter);
+app.use (launchesRouter);
 app.get ('/', (req, res) => {
   res.sendFile (path.join (__dirname, '..', 'public', 'index.html'));
 });
