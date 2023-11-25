e9e16b64dbcef487df181fd7132f2eb953035604
rh-pr <ruslana.hural.j@gmail.com>
2023-11-26 00:10:01 +0200

    made simple validation

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -10,6 +10,24 @@ function httpGetAllLaunches (req, res) {
 function httpAddNewLaunch (req, res) {
   // because in app we have use(express.json()) middleware
   const launch = req.body;
+
+  if (
+    !launch.mission ||
+    !launch.rocket ||
+    !launch.launchDate ||
+    !launch.destination
+  ) {
+    return res.status (400).json ({
+      error: 'Missing required launch property',
+    });
+  }
+
+  if (isNaN (launch.launchDate)) {
+    return res.status (400).json ({
+      error: 'Invalid launch date',
+    });
+  }
+
   launch.launchDate = new Date (launch.launchDate);
   addNewLaunches (launch);
   return res.status (200).json (launch);
