2e379f4eaf5d21a17ccc65bf20b7096c9116a321
rh-pr <ruslana.hural.j@gmail.com>
2024-02-17 23:17:54 +0200

    adding documents

--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -1,8 +1,8 @@
 const {getAllPlanets} = require ('../../models/planets.model');
 
-function httpGetAllPlanets (req, res) {
+async function httpGetAllPlanets (req, res) {
   console.log (req);
-  return res.status (200).json (getAllPlanets ()); // using the return to make sure that our function stops executing and only one response is ever set.
+  return res.status (200).json (await getAllPlanets ()); // using the return to make sure that our function stops executing and only one response is ever set.
 }
 
 module.exports = {
